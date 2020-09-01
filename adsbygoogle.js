aa00a5bd140335c83d0423215f2a42c4392cdcc9
droidxrx <61324913+droidxrx@users.noreply.github.com>
2020-09-01 12:36:08 +0700

    Update adsbygoogle.js

--- adsbygoogle.js
+++ adsbygoogle.js
@@ -1,1 +1,4 @@
-var e=document.createElement('div');e.id='234wasdfaw';document.getElementById('header-outer').appendChild(e);
+var e=document.createElement('div');
+e.id='drgsdfsdzrgd';
+e.style.display='none';
+document.body.appendChild(e);
